c3d519887fede2bedb8445b4965dfa426b8b6f37
mneedes <mneedes@gmail.com>
2024-02-27 10:17:29 -0800

    Add tagging, walking and 64-bit architecture support.

--- include/mos/allocator.h
+++ include/mos/allocator.h
@@ -28,22 +28,42 @@ typedef struct {
     u16       minBlockSize;
 } MosHeap;
 
-/// Initialize heap with an optional memory pool.
-///  Set pPool to NULL to defer adding initial pool.
+/// Callback function used for walking through allocations.
+/// pBlock is NULL and tag is 0 for an unallocated blocks.
+/// Default tag value is zero.
+typedef void (mosHeapCallbackFunc)(void * pBlock, u16 tag, u32 size);
+
+/// Initialize heap with the first memory pool
+///
 void mosInitHeap(MosHeap * pHeap, u16 alignment, u8 * pPool, u32 poolSize);
-/// Add memory pool to initialized heap
+
+/// Add additional memory pool to heap
 ///
 void mosAddHeapPool(MosHeap * pHeap, u8 * pPool, u32 poolSize);
+
 /// Allocate a block from the heap of a given size.
 /// Returns NULL on failure.
 void * mosAlloc(MosHeap * pHeap, u32 size);
+
 /// Reallocate a block from the heap to the given new size, preserving existing
-//  data (truncation occurs only if size decreases). If call fails existing block will
-//  NOT be freed and NULL will be returned.
+/// data (truncation occurs only if size decreases). If call fails existing block will
+//  NOT be freed and NULL will be returned. If pBlock is NULL, behavior is just like
+//  mosAlloc, if newSize is zero, then the block is freed and NULL returned.
 void * mosRealloc(MosHeap * pHeap, void * pBlock, u32 newSize);
+
 /// Return block back to the heap.
 ///
 void mosFree(MosHeap * pHeap, void * pBlock);
+
+/// Set the debug tag of an already allocated block.
+/// pBlock must have been returned by mosAlloc() or mosRealloc().
+/// The default block tag is zero.
+void mosTagAllocatedBlock(void * pBlock, u16 tag);
+
+/// Walk through heap, invoking callback on each allocated block or unallocated chunk.
+///
+void mosWalkHeap(MosHeap * pHeap, mosHeapCallbackFunc * pCallbackFunc);
+
 /// Get the max block size heap metric for evaluating fragmentation.
 ///
 u32 mosGetBiggestAvailableChunk(MosHeap * pHeap);
--- source/mos/allocator.c
+++ source/mos/allocator.c
@@ -1,5 +1,5 @@
 
-// Copyright 2019-2023 Matthew C Needes
+// Copyright 2019-2024 Matthew C Needes
 // You may not use this source file except in compliance with the
 // terms and conditions contained within the LICENSE file (the
 // "License") included under this distribution.
@@ -8,24 +8,6 @@
 
 /*
  * MOS General Purpose Allocator
- *
- * Allocated blocks:
- *
- *  |canary|size_p| size |      payload       |canary|size_p| size...
- *                       ^                    ^
- *                   alignment                +- next block
- *
- *    Actual alignment = max(requested alignment, pointer size)
- *    Implicit link sizes = sizeof(payload) + sizeof(Link)
- *    LSB of size/size_p gets set to indicate block is allocated
- *
- * Freed block:
- *
- *  |canary|size_p| size | (free-list link)  payload  |...
- *                       ^         ^                  ^
- *                   alignment     |                  +- next block
- *                                 |
- *   Payload of freed block contains explicit link
  *
  * Deterministic best-effort allocation using bins:
  *
@@ -42,11 +24,50 @@
  *      1 - 32-63
  *      2 - 64-127
  *         ...
- *     13 - 131072-262143
+ *     12 - 65536 - 131071
+ *     13 - 131072 (and above)
+ *
+ * Heap may be composed of multiple non-contiguous pools:
+ *
+ *   Pool format at initialization:
+ *     PAD
+ *       pointer to next pool
+ *       MosList bins[14];   // NOTE: bins are present only in first pool
+ *     PAD to guarantee block alignment after First Link
+ *       First Link
+ *       ...Free Space...
+ *       Last Link
+ *
+ * Allocated blocks:
+ *
+ *  |canary_tag|size_p| size |      payload       |canary_tag|size_p| size...
+ *                           ^                    ^
+ *                       alignment                +- next block
+ *
+ *    Actual alignment = max(requested alignment, pointer size)
+ *    Implicit link sizes = sizeof(payload) + sizeof(Link)
+ *    LSB of size/size_p gets set to indicate block is allocated
+ *
+ * Freed block:
+ *
+ *  |canary_tag|size_p| size | (free-list link)  payload  |...
+ *                           ^         ^                  ^
+ *                       alignment     |                  +- next block
+ *                                     |
+ *   Payload of freed block contains explicit link
  */
 
+#if defined(__SIZEOF_SIZE_T__)
+#if (__SIZEOF_SIZE_T__ == 8)
+  #define MOS_ARCHITECTURE_BITS 64
+#elif (__SIZEOF_SIZE_T__ == 4)
+  #define MOS_ARCHITECTURE_BITS 32
+#endif
+#endif
+
 enum {
-    HEAP_CANARY_VALUE = 0xe711dead,
+    CANARY_CHECKSUM   = 0xe711,
+    CANARY_DEFAULT    = CANARY_CHECKSUM << 16,
     MIN_PAYLOAD_SIZE  = sizeof(MosList),
     MIN_BIN_SHIFT     = 4,
     NUM_BINS          = 14,
@@ -54,26 +75,34 @@ enum {
 };
 
 typedef struct {
-    u32  canary;
-    u32  size_p;
-    u32  size;
+    u32  canary_tag;   /* combination canary/tag field */
+#if MOS_ARCHITECTURE_BITS == 64
+    u32  pad;
+#endif
+    u32  size_p;       /* size of previous block */
+    u32  size;         /* size of this block */
 } Link;
 
 typedef struct {
     Link link;
-    union { /* <--  Alignment guaranteed here */
+    union {            /* <--  Alignment guaranteed here */
         MosLink flLink;
         u8      payload[0];
     };
 } Block;
 
-#define BIN_NUM_FOR_SIZE(size)  (31 - __builtin_clz(size) - MIN_BIN_SHIFT)
-#define BIN_MASK_FOR_SIZE(size) (1 << BIN_NUM_FOR_SIZE(size))
-
+#if MOS_ARCHITECTURE_BITS == 32
 MOS_STATIC_ASSERT(Link, sizeof(Link) == 12);
 MOS_STATIC_ASSERT(Block, sizeof(Block) == 20);
+#elif MOS_ARCHITECTURE_BITS == 64
+MOS_STATIC_ASSERT(Link, sizeof(Link) == 16);
+MOS_STATIC_ASSERT(Block, sizeof(Block) == 32);
+#endif
 
-void mosInitHeap(MosHeap * pHeap, u16 alignment, u8 * pBot_, u32 poolSize) {
+#define BIN_MASK_FOR_SIZE(size) (1 << BinNumForSize(size))
+#define CANARY_CHECK(c)         (((c) & 0xffff) ^ ((c) >> 16) == CANARY_CHECKSUM)
+
+void mosInitHeap(MosHeap * pHeap, u16 alignment, u8 * pBot, u32 poolSize) {
     /* Alignment must be a power of 2, and at a minimum should be
      *   the pointer size. Smallest block must fit a Link, the free-list
      *   link and satisfy alignment requirements of payload. */
@@ -81,20 +110,27 @@ void mosInitHeap(MosHeap * pHeap, u16 alignment, u8 * pBot_, u32 poolSize) {
     pHeap->alignMask = alignment - 1;
     mosAssert((alignment & pHeap->alignMask) == 0);
     pHeap->minBlockSize = MOS_ALIGN32(MIN_PAYLOAD_SIZE + sizeof(Link), pHeap->alignMask);
+    /* Align bins to MosList alignment, leaving room for next pool pointer */
+    pHeap->pBins = (MosList *)MOS_ALIGN_PTR(pBot + sizeof(void *), sizeof(void *) - 1);
+    mosAssert((u8 *)(&pHeap->pBins[NUM_BINS]) - pBot < poolSize);
     /* Init free-list bins */
-    pHeap->pBins = (MosList *)MOS_ALIGN_PTR(pBot_, sizeof(void *) - 1);
-    mosAssert((u8 *)(&pHeap->pBins[NUM_BINS]) - pBot_ < poolSize);
     for (u32 ix = 0; ix < NUM_BINS; ix++) {
         mosInitList(&pHeap->pBins[ix]);
     }
-    poolSize -= ((u8 *)&pHeap->pBins[NUM_BINS] - pBot_);
-    pBot_ = (u8 *)&pHeap->pBins[NUM_BINS];
+    /* Init next pool pointer */
+    pBot = (u8 *)pHeap->pBins - sizeof(void *);
+    *((void **)pBot) = NULL;
     pHeap->binMask = 0;
     /* Free counters */
     pHeap->bytesFree    = 0;
     pHeap->minBytesFree = 0;
     mosInitMutex(&pHeap->mtx);
-    mosAddHeapPool(pHeap, pBot_, poolSize);
+    mosAddHeapPool(pHeap, pBot, poolSize);
+}
+
+MOS_INLINE u32 BinNumForSize(u32 size) {
+    u32 bin = 31 - __builtin_clz(size) - MIN_BIN_SHIFT;
+    return bin > (NUM_BINS - 1) ? NUM_BINS - 1 : bin;
 }
 
 static void RemoveFromFreeList(MosHeap * pHeap, Block * pBlock) {
@@ -105,26 +141,38 @@ static void RemoveFromFreeList(MosHeap * pHeap, Block * pBlock) {
 }
 
 static void AddToFreeList(MosHeap * pHeap, Block * pBlock) {
-    u32 bin = BIN_NUM_FOR_SIZE(pBlock->link.size);
+    u32 bin = BinNumForSize(pBlock->link.size);
     mosAddToFrontOfList(&pHeap->pBins[bin], &pBlock->flLink);
     pHeap->binMask |= (1 << bin);
 }
 
-void mosAddHeapPool(MosHeap * pHeap, u8 * pBot_, u32 poolSize) {
-    mosAssert(poolSize >= 128);
+void mosAddHeapPool(MosHeap * pHeap, u8 * pBot__, u32 poolSize) {
+    mosAssert(poolSize >= 256);
+    mosLockMutex(&pHeap->mtx);
+    u8 * pBot_ = (void *)MOS_ALIGN_PTR(pBot__, sizeof(void *) - 1);
+    if (pBot_ == ((u8 *)pHeap->pBins) - sizeof(void *)) {
+        /* First pool, skip bin array */
+        pBot_ = (u8 *)&pHeap->pBins[NUM_BINS];
+    } else {
+        /* Subsequent pool, add pool to end of list */
+        void ** ppNext = (void **)(((u8 *)pHeap->pBins) - sizeof(void *));
+        while (*ppNext != NULL) ppNext = *ppNext;
+        *ppNext = pBot_;
+        *((void **)pBot_) = NULL;
+        pBot_ += sizeof(void *);
+    }
+    poolSize -= (pBot_ - pBot__);
     Link * pLink = (Link *)MOS_ALIGN_PTR(pBot_ + sizeof(Link), pHeap->alignMask);
     Block * pBot = (Block *)(pLink - 1);
     pLink        = (Link *)MOS_ALIGN_PTR_DOWN(pBot_ + poolSize, pHeap->alignMask);
     Block * pTop = (Block *)(pLink - 1);
     /* Initialize implicit links */
-    pBot->link.canary = HEAP_CANARY_VALUE;
-    pBot->link.size_p = 0x1;
-    pBot->link.size   = (u8 *)pTop - (u8 *)pBot;
-    pTop->link.canary = HEAP_CANARY_VALUE;
-    pTop->link.size_p = pBot->link.size;
-    pTop->link.size   = 0x1;
-    mosAssert(BIN_NUM_FOR_SIZE(pBot->link.size) < NUM_BINS);
-    mosLockMutex(&pHeap->mtx);
+    pBot->link.canary_tag = CANARY_DEFAULT;
+    pBot->link.size_p     = 0x1;
+    pBot->link.size       = (u8 *)pTop - (u8 *)pBot;
+    pTop->link.canary_tag = CANARY_DEFAULT;
+    pTop->link.size_p     = pBot->link.size;
+    pTop->link.size       = 0x1;
     /* Add to size */
     pHeap->bytesFree    += pBot->link.size;
     pHeap->minBytesFree += pBot->link.size;
@@ -148,7 +196,7 @@ void * mosAlloc(MosHeap * pHeap, u32 size) {
             for (u32 try = 0; try < MAX_ALLOC_TRIES; try++, pLink = pLink->pNext) {
                 if (pLink == &pHeap->pBins[bin]) break;
                 pBlock = container_of(pLink, Block, flLink);
-                mosAssert(pBlock->link.canary == HEAP_CANARY_VALUE);
+                mosAssert(CANARY_CHECK(pBlock->link.canary_tag));
                 if (pBlock->link.size >= size) goto FOUND;
             }
             /* Otherwise split next available bin */
@@ -156,7 +204,7 @@ void * mosAlloc(MosHeap * pHeap, u32 size) {
             if (searchMask) {
                 bin = __builtin_ctz(searchMask);
                 pBlock = container_of(pHeap->pBins[bin].pNext, Block, flLink);
-                mosAssert(pBlock->link.canary == HEAP_CANARY_VALUE);
+                mosAssert(CANARY_CHECK(pBlock->link.canary_tag));
                 goto FOUND;
             }
         }
@@ -172,7 +220,7 @@ FOUND:
         pNextBlock->link.size_p = nextBlockSize;
         /* The new next block */
         pNextBlock = (Block *)((u8 *)pBlock + size);
-        pNextBlock->link.canary = HEAP_CANARY_VALUE;
+        pNextBlock->link.canary_tag = CANARY_DEFAULT;
         pNextBlock->link.size = nextBlockSize;
         /* Set size and mark allocation bit */
         pNextBlock->link.size_p = size + 1;
@@ -201,8 +249,8 @@ void * mosRealloc(MosHeap * pHeap, void * pBlock_, u32 newSize_) {
     }
     mosLockMutex(&pHeap->mtx);
     Block * pBlock = (Block *)((u8 *)pBlock_ - sizeof(Link));
-    /* Check for canary value and double-free */
-    mosAssert(pBlock->link.canary == HEAP_CANARY_VALUE);
+    /* Check for canary_tag value and double-free */
+    mosAssert(CANARY_CHECK(pBlock->link.canary_tag));
     mosAssert(pBlock->link.size & 0x1);
     u32 newSize = newSize_;
     if (newSize < MIN_PAYLOAD_SIZE) newSize = MIN_PAYLOAD_SIZE;
@@ -228,7 +276,7 @@ void * mosRealloc(MosHeap * pHeap, void * pBlock_, u32 newSize_) {
         pNextBlock->link.size_p = nextBlockSize;
         /* The new next block */
         pNextBlock = (Block *)((u8 *)pBlock + newSize);
-        pNextBlock->link.canary = HEAP_CANARY_VALUE;
+        pNextBlock->link.canary_tag = CANARY_DEFAULT;
         pNextBlock->link.size = nextBlockSize;
         /* Set size and mark allocation bit */
         pNextBlock->link.size_p = newSize + 1;
@@ -256,14 +304,14 @@ void mosFree(MosHeap * pHeap, void * pBlock_) {
     if (!pBlock_) return;
     Block * pBlock = (Block *)((u8 *)pBlock_ - sizeof(Link));
     mosLockMutex(&pHeap->mtx);
-    /* Check for canary value and double-free */
-    mosAssert(pBlock->link.canary == HEAP_CANARY_VALUE);
+    /* Check for canary_tag value and double-free */
+    mosAssert(CANARY_CHECK(pBlock->link.canary_tag));
     mosAssert(pBlock->link.size & 0x1);
     /* Clear allocation bit */
     pBlock->link.size--;
-    /* Check next canary value */
+    /* Check next canary_tag value */
     Block * pNext = (Block *)((u8 *)pBlock + pBlock->link.size);
-    mosAssert(pNext->link.canary == HEAP_CANARY_VALUE);
+    mosAssert(CANARY_CHECK(pNext->link.canary_tag));
     pHeap->bytesFree += pBlock->link.size;
     /* Find next and previous blocks and determine if allocated */
     Block * pPrev = NULL;
@@ -294,6 +342,39 @@ void mosFree(MosHeap * pHeap, void * pBlock_) {
     pNext->link.size_p = pBlock->link.size;
     /* Add block to free-list */
     AddToFreeList(pHeap, pBlock);
+    mosUnlockMutex(&pHeap->mtx);
+}
+
+void mosTagAllocatedBlock(void * pBlock_, u16 tag) {
+    Block * pBlock = (Block *)((u8 *)pBlock_ - sizeof(Link));
+    mosLockMutex(&pHeap->mtx);
+    mosAssert(CANARY_CHECK(pBlock->link.canary_tag));
+    mosAssert(pBlock->link.size & 0x1);
+    pBlock->link.canary_tag = tag | (CANARY_CHECKSUM ^ tag) << 16;
+    mosUnlockMutex(&pHeap->mtx);
+}
+
+void mosWalkHeap(MosHeap * pHeap, mosHeapCallbackFunc * pFunc) {
+    mosLockMutex(&pHeap->mtx);
+    void ** ppNext = (void **)(((u8 *)pHeap->pBins) - sizeof(void *));
+    u8 * pBot = (u8 *)&pHeap->pBins[NUM_BINS];
+    do {
+        Link * pLink  = (Link *)MOS_ALIGN_PTR(pBot + sizeof(Link), pHeap->alignMask);
+        pLink--;
+        do {
+           Block * pBlock = container_of(pLink, Block, flLink);
+           u32 nSize = pLink->size;
+           if (nSize & 0x1) {
+               nSize--;
+               (*pFunc)((void *)(pLink + 1), pLink->canary_tag & 0xffff, nSize);
+           } else {
+               (*pFunc)(NULL, 0, pLink->size);
+           }
+           pLink = (Link *)((u8 *)pLink + nSize);
+        } while (pLink->size != 1);
+        ppNext = *ppNext;
+        pBot = ((u8 *)ppNext) + sizeof(void *);
+    } while (ppNext != NULL);
     mosUnlockMutex(&pHeap->mtx);
 }
 
